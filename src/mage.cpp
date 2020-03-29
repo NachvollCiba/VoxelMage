f35a0cd0ee8bdb0b0175c4fbb08b2781e8f10525
Dennis Ciba <dennis.ciba@udo.edu>
2020-03-29 23:39:17 +0200

    Initial commit

---
+++ src/mage.cpp
@@ -0,0 +1,38 @@
+#include <exception>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+
+GLFWwindow* createWindow() {
+	// initialize GLFW
+	if (!glfwInit()) {
+		throw "Failed to initialize glfw3";
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(1920, 1080, "VoxelMage", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "FATAL ERROR: could not create GLFW Window");
+		glfwTerminate();
+		return NULL;
+	}
+	glfwMakeContextCurrent(window);
+
+	return window;
+}
+
+int main(int argc, char *argv[]) {
+	GLFWwindow* window = createWindow();
+
+	
+
+	return 0;
+}
+
